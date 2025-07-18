336f0d6f5f0e6b39b902fbe3090df11c98fa4e05
Pedro Ferreira <pedroesferreira@gmail.com>
2025-07-18 21:24:48 +0100

    improvements

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 const questions = [
     "What technical strengths and flaws have you seen in my work? Can you provide an example of each?",
     "Are there any technical aspects or areas I should focus on improving?",
-    "Have I been reliable and accountable in our daily tasks? Any instances where I have failed?",
+    "Have I been reliable and accountable in our daily tasks? Any instances where I have failed or went above and beyond?",
     "How do you view my contributions during team meetings or other group discussions?",
     "Where do you see opportunities for me to grow next? Where could I take more initiative in?",
     "What unique trait or quality do I bring to the team? How does it impact the team overall?",
